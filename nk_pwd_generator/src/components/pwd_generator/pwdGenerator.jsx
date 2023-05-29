a7aa28ecb0aa6cb8508d614331e23191a7f0effc
ckevin <kevin.chaffaux@ynov.com>
2023-05-30 00:45:24 +0200

    :sparkles: | finish project

--- nk_pwd_generator/src/components/pwd_generator/pwdGenerator.jsx
+++ nk_pwd_generator/src/components/pwd_generator/pwdGenerator.jsx
@@ -3,18 +3,22 @@ import { useState } from 'react';
 
 export const PwdGenerator = () => {
 
-    const [pwdLength,setPwdLength] = useState(3);
+    const [pwdLength,setPwdLength] = useState(4);
     const [pwdResult,setPwdResult] = useState("");
     const [pwdStength,setPwdStength] = useState("");
     const [uppercase, setUppercase] = useState(false);
     const [lowercase, setLowercase] = useState(false);
     const [numbers, setNumbers] = useState(false);
     const [symbols, setSymbols] = useState(false);
-    const [message, setMessage] = useState('');
+    const [dispStrength,setDispStrength] = useState(displayStrength(calculatePasswordStrength("")))
     const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
     const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const allNumbers = "0123456789"
-    const allSymbols = "&#@$£"
+    const allSymbols = "&#@$£+-*/.°="
+    var regexSymbols = /[\W_]/
+    var regexNumbers = /\d/
+    var regexLowercase = /[a-z]/
+    var regexUppercase = /[A-Z]/
 
     function calculatePassword () {
         let tempBase = ""
@@ -23,48 +27,87 @@ export const PwdGenerator = () => {
         if (lowercase){ tempBase += lowerAlphabet }
         if (numbers){ tempBase += allNumbers }
         if (symbols){ tempBase += allSymbols }
+        let hasNotUppercase = uppercase
+        let hasNotLowercase = lowercase
+        let hasNotSymbols = symbols
+        let hasNotNumbers = numbers
         if(tempBase != "" ){
-            for (let index = 0; index < pwdLength; index++) {
-                let randNumber =  Math.floor(Math.random() * tempBase.length);
-                result += tempBase[randNumber]
-                console.log("a");
+            while ((hasNotUppercase || hasNotLowercase || hasNotNumbers || hasNotSymbols)) {
+                result = ""
+                for (let index = 0; index < pwdLength; index++) {
+                    let randNumber =  Math.floor(Math.random() * tempBase.length);
+                    result += tempBase[randNumber]
+                }
+                if (uppercase) {
+                    if (regexUppercase.test(result)) {hasNotUppercase = false} else {hasNotUppercase = uppercase , hasNotLowercase = lowercase,hasNotNumbers = numbers, hasNotSymbols = symbols}
+                }
+                if (lowercase) {
+                    if (regexLowercase.test(result)) {hasNotLowercase = false}  else {hasNotUppercase = uppercase , hasNotLowercase = lowercase,hasNotNumbers = numbers, hasNotSymbols = symbols}
+                }
+                if (numbers) {
+                    if (regexNumbers.test(result)) {hasNotNumbers = false}  else {hasNotUppercase = uppercase , hasNotLowercase = lowercase,hasNotNumbers = numbers, hasNotSymbols = symbols}
+                }
+                if (symbols) {
+                    if (regexSymbols.test(result)) {hasNotSymbols = false}  else {hasNotUppercase = uppercase , hasNotLowercase = lowercase,hasNotNumbers = numbers, hasNotSymbols = symbols}
+                }
             }
+        } else {
+            result = "No parameters selected"
         }
         return result
     }
 
-    function calculatePasswordStrength () {
+    function calculatePasswordStrength (password) {
         let result = 0
         result +=  Number(pwdLength)
-        var regexSybols = /[\W_]/;
-        var regexNumbers = /\d/;
-        var regexLowercase = /[a-z]/;
-        var regexUppercase = /[A-Z]/;
-        if (regexSybols.test(pwdResult)) {
-            result += 5
+        if (password == "No parameters selected" || password == "") {
+            return ""
         }
-        if (regexNumbers.test(pwdResult)) {
-            result += 4
-        }
-        if (regexLowercase.test(pwdResult)) {
-            result += 3
-        }
-        if (regexUppercase.test(pwdResult)) {
-            result += 3
-        }
+        if (regexSymbols.test(password)) {result += 7 }
+        if (regexNumbers.test(password)) {result += 4 }
+        if (regexLowercase.test(password)) {result += 2}
+        if (regexUppercase.test(password)) {result += 2}
         result = ((result * 40) / 30)
         if (result <= 13){
             return "TOO LOW"
-        }     
-        else if (result <= 22) {
+        } else if (result <= 22) {
             return "EASY"
-        }   
-        else if (result <= 28) {
+        } else if (result <= 28) {
             return "MEDIUM"
-        }  
-        else if (result <= 40){
+        } else if (result <= 40){
             return "HARD"
+        } else {
+            return ""
         }
+    }
+
+    function displayStrength (strength) {
+        let color = ""
+        let coloredNb = 0
+        if (strength == "TOO LOW") {
+            coloredNb = 1
+        } else if (strength == "EASY") {
+            color = "rgb(230, 117, 72)"
+            coloredNb = 2
+        } else if (strength == "MEDIUM") {
+            color = "rgb(223, 223, 126)"
+            coloredNb = 3
+        } else if (strength == "HARD") {
+            color = "lightgreen"
+            coloredNb = 4
+        } 
+        let result = []
+        for (let index = 0; index < coloredNb; index++) {
+            result.push(<span key={index} className='strengthBar' style={{backgroundColor:color,border:`2px solid ${color}`}}></span>)
+        }
+        for (let index = 0; index < 4 - coloredNb; index++) {
+            result.push(<span key={4-index} className='strengthBar' style={{border:`2px solid ${color}`}}></span>)
+        }
+        return (    
+            <div className='rowContainer containerDisplayStrength'>
+                {result}
+            </div>
+        )       
     }
 
     function handleChangeRange(e) {
@@ -73,7 +116,7 @@ export const PwdGenerator = () => {
         return {value: e.target.value}
     }
 
-    function handleChange(e,name) {
+    function handleChangeChecked(e,name) {
         let isChecked = e.target.checked;
         switch (name) {
             case "symbols":
@@ -94,52 +137,53 @@ export const PwdGenerator = () => {
 
     function handleSubmit(e) {
       e.preventDefault();
-      setMessage(`Parameters : ${uppercase} ${lowercase} ${numbers} ${symbols} Range : ${pwdLength}`);
-      setPwdResult(calculatePassword())
-      setPwdStength(calculatePasswordStrength())
-      console.log(pwdStength);
-    };
+      const password = calculatePassword()
+      setPwdResult(password)
+      const pwdSten = calculatePasswordStrength(password)
+      setPwdStength(pwdSten)
+      setDispStrength(displayStrength(pwdSten))
+    }
 
     return (
-        
-        <div className='containerPwdGen columnContainer'>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-            <form onSubmit={handleSubmit} className='formStyle columnContainer'>
-                {/* <h2>{message}</h2> */}
-                <div className='rowContainer containerHeader'>
-                    <h2>{pwdResult}</h2>
-                    <span class="googleIcon material-symbols-outlined">content_copy</span>
-                </div>
-                <div className='content columnContainer'>
-                    <input type="range" onChange={e => handleChangeRange(e)} min={"5"} max={"15"} defaultValue={"2"} step={"1"}/>
-                    <div className='checkboxContainer columnContainer'>
-                        <div className='rowContainer containerInCheck'>
-                            <input type="checkbox" onChange={e => handleChange(e ,  "uppercase")}  name="Uppercase" id="" />
-                            <label htmlFor="">Uppercase</label>
-                        </div>
-                        <div className='rowContainer containerInCheck'>
-                            <input type="checkbox" onChange={e => handleChange(e ,  "lowercase")} name="Lowercase" id="" />
-                            <label htmlFor="">Lowercase</label>
-                        </div>
-                        <div className='rowContainer containerInCheck'>
-                            <input type="checkbox" onChange={e => handleChange(e ,  "numbers")} name="Numbers" id="" />
-                            <label htmlFor="">Numbers</label>
-                        </div>
-                        <div className='rowContainer containerInCheck'>
-                            <input type="checkbox" onChange={e => handleChange(e ,  "symbols")} name="Symbols" id="" />
-                            <label htmlFor="">Symbols</label>
-                        </div>
+        <div className='columnContainer alignCenter containerPage'>
+            <div className='containerPwdGen columnContainer'>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+                <form onSubmit={handleSubmit} className='formStyle columnContainer'>
+                    <div className='rowContainer containerHeader'>
+                        <h2>{pwdResult}</h2>
+                        <span onClick={() => navigator.clipboard.writeText(pwdResult)} className="googleIcon material-symbols-outlined">content_copy</span>
                     </div>
-                    <div className='rowContainer containerStength'>
-                        <h3>STENGTH</h3>
-                        <div className='rowContainer'>
-                            <h3>{pwdStength}</h3>
+                    <div className='content columnContainer'>
+                        <input type="range" onChange={e => handleChangeRange(e)} min={"5"} max={"15"} defaultValue={"4"} step={"1"}/>
+                        <div className='checkboxContainer columnContainer'>
+                            <div className='rowContainer containerInCheck'>
+                                <input type="checkbox" onChange={e => handleChangeChecked(e , "uppercase")}  name="Uppercase"/>
+                                <label htmlFor="">Uppercase</label>
+                            </div>
+                            <div className='rowContainer containerInCheck'>
+                                <input type="checkbox" onChange={e => handleChangeChecked(e , "lowercase")} name="Lowercase"/>
+                                <label htmlFor="">Lowercase</label>
+                            </div>
+                            <div className='rowContainer containerInCheck'>
+                                <input type="checkbox" onChange={e => handleChangeChecked(e , "numbers")} name="Numbers"/>
+                                <label htmlFor="">Numbers</label>
+                            </div>
+                            <div className='rowContainer containerInCheck'>
+                                <input type="checkbox" onChange={e => handleChangeChecked(e , "symbols")} name="Symbols"/>
+                                <label htmlFor="">Symbols</label>
+                            </div>
                         </div>
+                        <div className='rowContainer containerStength '>
+                            <h3>STENGTH</h3>
+                            <div className='rowContainer alignCenter containerStengthGap'>
+                                <h3>{pwdStength}</h3>
+                                {dispStrength}
+                            </div>
+                        </div>
+                        <button className='sumbmitBtn' type="submit">Generate</button>
                     </div>
-                    <button className='sumbmitBtn' type="submit">Generate</button>
-                </div>
-              
-            </form>
+                </form>
+            </div>
         </div>
     )
 }
--- nk_pwd_generator/src/main.jsx
+++ nk_pwd_generator/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/app/App'
 import { PwdGenerator } from './components/pwd_generator/pwdGenerator'
 import './index.css'
 
